8452c9bd625ee87207c24ae32088188f40c97a00
Raphael Jatobá Passos <85450355+raphajpassos@users.noreply.github.com>
2021-08-20 09:41:17 -0300

    Add files via upload
    The starter files

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Selecting elements by ID we can use .GetElementById
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+
+score0.textContent = 0;
+score1.textContent = 0;
